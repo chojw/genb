6bbd6e3b73ce19dd05f355dee2db993fcaf43785
Jae Won Cho <57617399+chojw@users.noreply.github.com>
2023-03-31 22:53:16 +0900

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,135 @@
+from __future__ import print_function
+
+import errno
+import os
+import numpy as np
+# from PIL import Image
+import torch
+import torch.nn as nn
+import csv
+import base64
+import time
+import sys
+
+EPS = 1e-7
+
+
+def assert_eq(real, expected):
+    assert real == expected, '%s (true) vs %s (expected)' % (real, expected)
+
+
+def assert_array_eq(real, expected):
+    assert (np.abs(real-expected) < EPS).all(), \
+        '%s (true) vs %s (expected)' % (real, expected)
+
+
+def load_folder(folder, suffix):
+    imgs = []
+    for f in sorted(os.listdir(folder)):
+        if f.endswith(suffix):
+            imgs.append(os.path.join(folder, f))
+    return imgs
+
+
+
+def weights_init(m):
+    """custom weights initialization."""
+    cname = m.__class__
+    if cname == nn.Linear or cname == nn.Conv2d or cname == nn.ConvTranspose2d:
+        m.weight.data.normal_(0.0, 0.02)
+    elif cname == nn.BatchNorm2d:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+    else:
+        print('%s is not initialized.' % cname)
+
+
+def init_net(net, net_file):
+    if net_file:
+        net.load_state_dict(torch.load(net_file))
+    else:
+        net.apply(weights_init)
+
+
+def create_dir(path):
+    if not os.path.exists(path):
+        try:
+            os.makedirs(path)
+        except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                raise
+
+
+class Logger(object):
+    def __init__(self, output_name):
+        dirname = os.path.dirname(output_name)
+        if not os.path.exists(dirname):
+            os.mkdir(dirname)
+
+        self.log_file = open(output_name, 'w')
+        self.infos = {}
+
+    def append(self, key, val):
+        vals = self.infos.setdefault(key, [])
+        vals.append(val)
+
+    def log(self, extra_msg=''):
+        msgs = [extra_msg]
+        for key, vals in self.infos.iteritems():
+            msgs.append('%s %.6f' % (key, np.mean(vals)))
+        msg = '\n'.join(msgs)
+        self.log_file.write(msg + '\n')
+        self.log_file.flush()
+        self.infos = {}
+        return msg
+
+    def write(self, msg):
+        self.log_file.write(msg + '\n')
+        self.log_file.flush()
+        print(msg)
+
+
+csv.field_size_limit(sys.maxsize)
+FIELDNAMES = ["img_id", "img_h", "img_w", "objects_id", "objects_conf",
+              "attrs_id", "attrs_conf", "num_boxes", "boxes", "features"]
+
+
+def load_obj_tsv(fname, topk=None):
+    """Load object features from tsv file.
+
+    :param fname: The path to the tsv file.
+    :param topk: Only load features for top K images (lines) in the tsv file.
+        Will load all the features if topk is either -1 or None.
+    :return: A list of image object features where each feature is a dict.
+        See FILENAMES above for the keys in the feature dict.
+    """
+    data = []
+    start_time = time.time()
+    print("Start to load Faster-RCNN detected objects from %s" % fname)
+    with open(fname) as f:
+        reader = csv.DictReader(f, FIELDNAMES, delimiter="\t")
+        for i, item in enumerate(reader):
+
+            for key in ['img_h', 'img_w', 'num_boxes']:
+                item[key] = int(item[key])
+            
+            boxes = item['num_boxes']
+            decode_config = [
+                ('objects_id', (boxes, ), np.int64),
+                ('objects_conf', (boxes, ), np.float32),
+                ('attrs_id', (boxes, ), np.int64),
+                ('attrs_conf', (boxes, ), np.float32),
+                ('boxes', (boxes, 4), np.float32),
+                ('features', (boxes, -1), np.float32),
+            ]
+            for key, shape, dtype in decode_config:
+                item[key] = np.frombuffer(base64.b64decode(item[key]), dtype=dtype)
+                item[key] = item[key].reshape(shape)
+                item[key].setflags(write=False)
+
+            data.append(item)
+            if topk is not None and len(data) == topk:
+                break
+    elapsed_time = time.time() - start_time
+    print("Loaded %d images in file %s in %d seconds." % (len(data), fname, elapsed_time))
+    return data
