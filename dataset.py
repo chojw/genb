05ef9edb9a15e63c8473d1da9d61b96aff00fb7a
Jae Won Cho <57617399+chojw@users.noreply.github.com>
2023-04-07 15:08:09 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -7,61 +7,11 @@ from torch.utils.data import DataLoader
 
 from dataset import Dictionary, VQAFeatureDataset
 import base_model
+from base_model import GenB, Discriminator
 
-from torch.autograd import Variable
-
-from tqdm import tqdm
-
-
-
-def compute_score_with_logits(logits, labels):
-    logits = torch.argmax(logits, 1)
-    one_hots = torch.zeros(*labels.size()).cuda()
-    one_hots.scatter_(1, logits.view(-1, 1), 1)
-    scores = (one_hots * labels)
-    return scores
-
-
-def evaluate(model, dataloader, qid2type):
-    score = 0
-    upper_bound = 0
-    score_yesno = 0
-    score_number = 0
-    score_other = 0
-    total_yesno = 0
-    total_number = 0
-    total_other = 0 
-
-    for v, q, a, qids in tqdm(dataloader, ncols=100, total=len(dataloader), desc="eval"):
-        v = Variable(v, requires_grad=False).cuda()
-        q = Variable(q, requires_grad=False).cuda()
-        pred = model(v, q)
-        batch_score = compute_score_with_logits(pred, a.cuda()).cpu().numpy().sum(1)
-        score += batch_score.sum()
-        upper_bound += (a.max(1)[0]).sum()
-        qids = qids.detach().cpu().int().numpy()
-        for j in range(len(qids)):
-            qid = qids[j]
-            typ = qid2type[str(qid)]
-            if typ == 'yes/no':
-                score_yesno += batch_score[j]
-                total_yesno += 1
-            elif typ == 'other':
-                score_other += batch_score[j]
-                total_other += 1
-            elif typ == 'number':
-                score_number += batch_score[j]
-                total_number += 1
-            else:
-                print('Hahahahahahahahahahaha')
-
-    score = score / len(dataloader.dataset)
-    upper_bound = upper_bound / len(dataloader.dataset)
-    score_yesno /= total_yesno
-    score_other /= total_other
-    score_number /= total_number
-
-    return score, upper_bound, score_yesno, score_other, score_number
+from train import train
+import utils
+import click
 
 
 def parse_args():
@@ -71,11 +21,14 @@ def parse_args():
     parser.add_argument('--cache_features', default=False, help="Cache image features in RAM. Makes things much faster"
                         "especially if the filesystem is slow, but requires at least 48gb of RAM")
     parser.add_argument('--dataset', default='cpv2', choices=["v2", "cpv2", "cpv1"], help="Run on VQA-2.0 instead of VQA-CP 2.0")
+    parser.add_argument('--eval_each_epoch', default=True,help="Evaluate every epoch, instead of at the end")
+    parser.add_argument('--epochs', type=int, default=25)
     parser.add_argument('--num_hid', type=int, default=1024)
     parser.add_argument('--model', type=str, default='baseline0_newatt')
+    parser.add_argument('--output', type=str, default='logs/exp0')
     parser.add_argument('--batch_size', type=int, default=512)
     parser.add_argument('--seed', type=int, default=1111, help='random seed')
-    parser.add_argument('--load_checkpoint_path', type=str, default='best_model')
+    parser.add_argument('--load_checkpoint_path', type=str, default=None)
     args = parser.parse_args()
     return args
 
@@ -83,39 +36,67 @@ def parse_args():
 def main():
     args = parse_args()
     dataset=args.dataset
+    args.output=os.path.join('logs',args.output)
+    if not os.path.isdir(args.output):
+        utils.create_dir(args.output)
+    else:
+        if click.confirm('Exp directory already exists in {}. Erase?'
+                                 .format(args.output, default=False)):
+            os.system('rm -r ' + args.output)
+            utils.create_dir(args.output)
+
+        else:
+            if args.load_checkpoint_path is None:
+                os._exit(1)
+
 
     if dataset=='cpv1':
         dictionary = Dictionary.load_from_file('data/dictionary_v1.pkl')
     elif dataset=='cpv2' or dataset=='v2':
         dictionary = Dictionary.load_from_file('data/dictionary.pkl')
 
+    print("Building train dataset...")
+    train_dset = VQAFeatureDataset('train', dictionary, dataset=dataset,
+                                   cache_image_features=args.cache_features)
+
     print("Building test dataset...")
     eval_dset = VQAFeatureDataset('val', dictionary, dataset=dataset,
-                                cache_image_features=args.cache_features)
+                                  cache_image_features=args.cache_features)
 
     # Build the model using the original constructor
     constructor = 'build_%s' % args.model
-    model = getattr(base_model, constructor)(eval_dset, args.num_hid).cuda()
-    
+    model = getattr(base_model, constructor)(train_dset, args.num_hid).cuda()
+    genb = GenB(num_hid=1024, dataset=train_dset).cuda()
+    discriminator = Discriminator(num_hid=1024, dataset=train_dset).cuda()
+    if dataset=='cpv1':
+        model.w_emb.init_embedding('data/glove6b_init_300d_v1.npy')
+        genb.w_emb.init_embedding('data/glove6b_init_300d_v1.npy')
+    elif dataset=='cpv2' or dataset=='v2':
+        model.w_emb.init_embedding('data/glove6b_init_300d.npy')
+        genb.w_emb.init_embedding('data/glove6b_init_300d.npy')
+
     with open('util/qid2type_%s.json'%args.dataset,'r') as f:
         qid2type=json.load(f)
 
-    ckpt = torch.load(os.path.join(args.load_checkpoint_path, 'model.pth'))
-    model.load_state_dict(ckpt)
-    print('Loaded Model!')
+    if args.load_checkpoint_path is not None:
+        ckpt = torch.load(os.path.join('logs', args.load_checkpoint_path, 'model.pth'))
+        model_dict = model.state_dict()
+        ckpt = {k: v for k, v in ckpt.items() if k in model_dict}
+        model_dict.update(model_dict)
+        model.load_state_dict(model_dict)
 
     model=model.cuda()
-    model.eval()
     batch_size = args.batch_size
 
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed(args.seed)
     torch.backends.cudnn.benchmark = True
 
+    train_loader = DataLoader(train_dset, batch_size, shuffle=True, num_workers=0)
     eval_loader = DataLoader(eval_dset, batch_size, shuffle=False, num_workers=0)
-    eval_score, bound, yn, other, num = evaluate(model, eval_loader, qid2type)
-    print('\teval score: %.2f (%.2f)' % (100 * eval_score, 100 * bound))
-    print('\tyn score: %.2f other score: %.2f num score: %.2f' % (100 * yn, 100 * other, 100 * num))
+
+    print("Starting training...")
+    train(model, genb, discriminator, train_loader, eval_loader, args,qid2type)
 
 if __name__ == '__main__':
     main()
